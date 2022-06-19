f7921d32d118a6bdae985f3e04493388da4231da
armanharut98506 <armanharut7117@gmail.com>
2022-06-19 18:45:58 +0400

    Solution for task3

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
-package homework
+package main
+
+import "fmt"
 
 func reverse(input []int64) (result []int64) {
 	for i, j := len(input) - 1, 0; i >= 0; i, j = i-1, j+1 {
 		result = append(result, input[i])
 	}
 	return result
+}
+
+func main() {
+	input := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(reverse(input))
 }
